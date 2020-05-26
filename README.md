2704f0c5ea8e2551615b125516726ae24ba9342f
Richard Whitehead <richard@moogsoft.com>
2020-05-26 12:27:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+![Moogsoft Logo](https://www.moogsoft.com/wp-content/uploads/2017/02/moog-logo.png)
+
+## Moogsoft Express bulk config change utility
+
+Leverages the Config API service to make bulk changes, using regex search capability
+
+The utility:
+
+* Queries the configuration for artifacts that match a regular expression
+* Applies further (optional) conditions
+* Applies changes to all matching configs
+
+# Installation
+
+`$ npm install moog-bulkconfig`
+`$ npm link moog-bulkconfig`
+
+## Usage
+
+```
+express-bulkconfig [-d] [-y] [-j] [-l debug] [-c config_file] [-h]
+    --dryrun:            Show what changes would be made, but don't do it.
+    --yaml:              Dump the entire seach results as a YAML file.
+    --json:              Dump the entire seach results as a YAML file.
+    --loglevel debug:    Be more verbose
+    --conf config_file:  Specifiy a config file (default is ./mbc-config.yaml)
+    --help:              The usage message
+```
+
+# The config file
+
+```JavaScript
+
+---
+apikey: 'foo_12345667890ljyg76dcOGYSJYGY7bhd7VB7vbfGHjyggy' 
+
+owner: 'acme.*EC2:'
+
+patchquery:
+  when:
+    sigma: 4
+    holdfor: 1
+  then:
+    sigma: 8
+```
+
+In the above example, all configutions where the `owner` key includes the string `acme` and also contains the string `EC2:` will be checked.
+
+If the configurations also have the `sigma` value 4, and `holdfor` 1, than the `sigma` value will be set to 8.
+
+See the API documenation for further information.
---
+++ index.js
@@ -0,0 +1,199 @@
+#!/usr/bin/env node
+/** **************************************************************************
+ *
+ *  Contents of file Copyright (c) Moogsoft Inc 2017-2020
+ *
+ *  Utility to make bulk config changes to an integration
+ *
+ *************************************************************************** */
+
+'use strict';
+
+const https = require('https');
+const url = require('url');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const getopts = require('getopts');
+
+const hostname = 'api.moogsoft.ai';
+const protocol = 'https';
+
+const cliopts = getopts(process.argv.slice(2), {
+    alias: {
+        loglevel: 'l',
+        dryrun: 'd',
+        conf: 'c',
+        yaml: 'y',
+        json: 'j',
+        help: 'h',
+    },
+    default: {
+        loglevel: 'warn',
+        dryrun: false,
+        conf: './mbc-config.yaml',
+        save: false,
+    },
+});
+
+if (cliopts.help) {
+    console.log('Usage: moog-bulkconfig [-d] [-s] [-y] [-l debug] [-c config_file] [-h]');
+    process.exit(0);
+}
+
+if (!fs.existsSync(cliopts.conf)) {
+    console.error(`ERROR: config file not found: ${cliopts.conf}`);
+    process.exit(2);
+}
+const configfile = yaml.safeLoad(fs.readFileSync(cliopts.conf, 'utf8'));
+
+//
+// Pull secrets, search parameters, and patch parameters
+//
+const apikey = configfile.apikey ? configfile.apikey : null;
+const owner = configfile.owner ? configfile.owner : 'NULLSEARCH';
+const patchquery = configfile.patchquery ? configfile.patchquery : {};
+
+const options = {
+    headers: {
+        apikey,
+        'Content-Type': 'application/json',
+    },
+};
+
+function debug(msg) {
+    if (cliopts.loglevel === 'debug') console.log(msg);
+}
+
+//
+// Define the query
+//
+const query = {
+    owner,
+    component: 'collector-metric-config',
+    fuzzy: true,
+    attributes: ['owner', 'detector'],
+};
+
+//
+// Build the URI with query string
+//
+query.attributes = `["${query.attributes.join('","')}"]`;
+const searchUrl = url.format({
+    hostname,
+    query,
+    protocol,
+    pathname: '/express/v1/config/search',
+});
+
+//
+// Define PATCH options
+//
+const patchOpts = {
+    hostname,
+    headers: options.headers,
+    method: 'PATCH',
+    path: '/express/v1/config',
+};
+
+//
+// Send a PATCH request to API
+//
+function patchConfig(pOpts, config) {
+    console.log(`applying: ${JSON.stringify(config)}`);
+    const request = https.request(pOpts, (res) => {
+        const chunks = [];
+        res.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+
+        res.on('end', () => {
+            const body = Buffer.concat(chunks);
+            debug(body.toString());
+        });
+    });
+    request.on('error', (error) => { console.log(error); process.exit(2); });
+
+    request.write(JSON.stringify(config));
+
+    request.end();
+}
+
+function saveYaml(saveObj) {
+    fs.writeFileSync('saved.yaml', yaml.safeDump(saveObj));
+}
+
+function evalQuery(d, q) {
+    if (q === {}) return true;
+    let MATCH = true;
+    Object.entries(q).forEach(([key, value]) => {
+        if (d[key] !== value) {
+            MATCH = false;
+        }
+    });
+    return MATCH;
+}
+
+function changeConfig(d, q) {
+    Object.entries(q).forEach(([key, value]) => {
+        d[key] = value;
+    });
+}
+
+//
+// Process the output of our search
+//
+function processBody(body) {
+    const resbody = JSON.parse(body);
+
+    resbody.data.forEach((m) => {
+        //
+        // This is a good place to filter metrics
+        //
+        if (evalQuery(m.detector, patchquery.when)) {
+            //
+            // This is where you mutate the config
+            //
+            console.log(`Matching metric: owner: ${m.owner}`);
+            console.log(`detector: ${JSON.stringify(m.detector)}`);
+            changeConfig(m.detector, patchquery.then);
+
+            //
+            // Build the PATCH payload
+            //
+            const payload = {
+                owner: m.owner,
+                component: 'collector-metric-config',
+                attributes: {
+                    detector: m.detector,
+                },
+            };
+            if (!cliopts.dryrun) patchConfig(patchOpts, payload);
+        }
+    });
+    if (cliopts.yaml) saveYaml(resbody.data);
+    if (cliopts.json) fs.writeFileSync('saved.json', JSON.stringify(resbody.data));
+}
+
+//
+// Send a GET request to the search endpoint
+//
+function searchConfig(sUrl, sOpts) {
+    console.log('==== Searching for metric configs ====');
+    debug(query);
+    debug(patchquery);
+    https.get(sUrl, sOpts, (res) => {
+        const accumulator = [];
+
+        res.on('data', (d) => {
+            accumulator.push(d);
+        });
+        res.on('end', () => {
+            const fullbody = Buffer.concat(accumulator);
+            processBody(fullbody);
+        });
+    }).on('error', (e) => {
+        console.error(e);
+    });
+}
+
+searchConfig(searchUrl, options);
